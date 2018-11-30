ddb8859b126d2f37d3ca80f2f4d4c8de7ca858d1
FranKo <franko.2365@gmail.com>
2018-11-30 00:28:55 -0400

    Cambios en las respuestas de edades - Sistema experto

--- app/src/main/java/com/example/franko/expertwisc/ExpertSystem/SistemaExperto.java
+++ app/src/main/java/com/example/franko/expertwisc/ExpertSystem/SistemaExperto.java
@@ -84,112 +84,199 @@ public class SistemaExperto {
 
 
         //IRP
-        Rule reglaIRP6 = new Rule(baseDeReglas, "reglaICV6",
+        Rule reglaIRP6 = new Rule(baseDeReglas, "reglaIRP6",
                 new Clause[]{
-                        new Clause(Area,igual,"ICV"),
+                        new Clause(Area,igual,"IRP"),
                         new Clause(Edad,menor,"8")
                 },
-                new Clause(Resultado,igual,"PLE, PROLEC, VADS")
+                new Clause(Resultado,igual,"PROCALCULO, BENDER, DFH")
         );
 
-        Rule reglaIRP8 = new Rule(baseDeReglas, "reglaICV8",
+        Rule reglaIRP8 = new Rule(baseDeReglas, "reglaIRP8",
                 new Clause[]{
-                        new Clause(Area,igual,"ICV"),
+                        new Clause(Area,igual,"IRP"),
                         new Clause(Edad,menor,"10")
                 },
-                new Clause(Resultado,igual,"PROLEC, LEE")
+                new Clause(Resultado,igual,"PROCALCULO, BENDER, DFHE")
         );
-        Rule reglaIRP10 = new Rule(baseDeReglas, "reglaICV10",
+        Rule reglaIRP10 = new Rule(baseDeReglas, "reglaIRP10",
                 new Clause[]{
-                        new Clause(Area,igual,"ICV"),
+                        new Clause(Area,igual,"IRP"),
                         new Clause(Edad,menor,"12")
                 },
-                new Clause(Resultado,igual,"LEE, VADS")
+                new Clause(Resultado,igual,"BENDER, DFH")
         );
-        Rule reglaIRP12 = new Rule(baseDeReglas, "reglaICV12",
+        Rule reglaIRP12 = new Rule(baseDeReglas, "reglaIRP12",
                 new Clause[]{
-                        new Clause(Area,igual,"ICV"),
+                        new Clause(Area,igual,"IRP"),
                         new Clause(Edad,menor,"14")
                 },
-                new Clause(Resultado,igual,"PROLEC-SE, VADS")
+                new Clause(Resultado,igual,"BENDER, DFH")
         );
-        Rule reglaIRP14 = new Rule(baseDeReglas, "reglaICV14",
+        Rule reglaIRP14 = new Rule(baseDeReglas, "reglaIRP14",
                 new Clause[]{
-                        new Clause(Area,igual,"ICV"),
+                        new Clause(Area,igual,"IRP"),
                         new Clause(Edad,menor,"15")
                 },
-                new Clause(Resultado,igual,"PROLEC-SE")
+                new Clause(Resultado,igual,"PMA")
         );
-        Rule reglaIRP15 = new Rule(baseDeReglas, "reglaICV15",
+        Rule reglaIRP15 = new Rule(baseDeReglas, "reglaIRP15",
                 new Clause[]{
-                        new Clause(Area,igual,"ICV"),
+                        new Clause(Area,igual,"IRP"),
                         new Clause(Edad,mayor,"14")
                 },
-                new Clause(Resultado,igual,"PROLEC-SE")
+                new Clause(Resultado,igual,"PMA")
         );
 
-
-
-
-
-
-
-
-        Rule reglaICV1 = new Rule(baseDeReglas, "reglaDos",
+        //IMO
+        Rule reglaIMO6 = new Rule(baseDeReglas, "reglaIMO6",
                 new Clause[]{
-                    new Clause(Area,igual,"ICV"),
-                        new Clause(Edad,mayor,"9")
+                        new Clause(Area,igual,"IMO"),
+                        new Clause(Edad,menor,"8")
                 },
-                    new Clause(Resultado,igual,"PROLEC")
+                new Clause(Resultado,igual,"ABC, VADS")
         );
 
-        Rule reglaTres = new Rule(baseDeReglas, "reglaTres",
+        Rule reglaIMO8 = new Rule(baseDeReglas, "reglaIMO8",
                 new Clause[]{
-                    new Clause(Area,igual,"IRP"),
-                        new Clause(Edad,menor,"7")
+                        new Clause(Area,igual,"IMO"),
+                        new Clause(Edad,menor,"10")
                 },
-                    new Clause(Resultado,igual,"BENDER")
+                new Clause(Resultado,igual,"VADS")
+        );
+        Rule reglaIMO10 = new Rule(baseDeReglas, "reglaIMO10",
+                new Clause[]{
+                        new Clause(Area,igual,"IMO"),
+                        new Clause(Edad,menor,"12")
+                },
+                new Clause(Resultado,igual,"VADS")
+        );
+        Rule reglaIMO12 = new Rule(baseDeReglas, "reglaIMO12",
+                new Clause[]{
+                        new Clause(Area,igual,"IMO"),
+                        new Clause(Edad,menor,"14")
+                },
+                new Clause(Resultado,igual,"VADS")
+        );
+        Rule reglaIMO14 = new Rule(baseDeReglas, "reglaIMO14",
+                new Clause[]{
+                        new Clause(Area,igual,"IMO"),
+                        new Clause(Edad,menor,"15")
+                },
+                new Clause(Resultado,igual,"")
+        );
+        Rule reglaIMO15 = new Rule(baseDeReglas, "reglaIMO15",
+                new Clause[]{
+                        new Clause(Area,igual,"IMO"),
+                        new Clause(Edad,mayor,"14")
+                },
+                new Clause(Resultado,igual,"")
         );
 
-        Rule reglaCuatro = new Rule(baseDeReglas, "reglaCuatro",
+        //IVP
+        Rule reglaIVP6 = new Rule(baseDeReglas, "reglaIVP6",
                 new Clause[]{
-                    new Clause(Area,igual,"IRP"),
-                        new Clause(Edad,mayor,"6")
+                        new Clause(Area,igual,"IVP"),
+                        new Clause(Edad,menor,"8")
                 },
-                    new Clause(Resultado,igual,"TEPSI")
+                new Clause(Resultado,igual,"PROCALCULO, BENDER")
         );
 
-        Rule reglaCinco = new Rule(baseDeReglas, "reglaCinco",
+        Rule reglaIVP8 = new Rule(baseDeReglas, "reglaIVP8",
                 new Clause[]{
-                    new Clause(Area,igual,"IMO"),
-                        new Clause(Edad,menor,"7")
+                        new Clause(Area,igual,"IVP"),
+                        new Clause(Edad,menor,"10")
                 },
-                    new Clause(Resultado,igual,"Pre-Cálculo")
+                new Clause(Resultado,igual,"RAVEN-C, BENDER, PROCALCULO")
+        );
+        Rule reglaIVP10 = new Rule(baseDeReglas, "reglaIVP10",
+                new Clause[]{
+                        new Clause(Area,igual,"IVP"),
+                        new Clause(Edad,menor,"12")
+                },
+                new Clause(Resultado,igual,"RAVEN-C, BENDER")
+        );
+        Rule reglaIVP12 = new Rule(baseDeReglas, "reglaIVP12",
+                new Clause[]{
+                        new Clause(Area,igual,"IVP"),
+                        new Clause(Edad,menor,"14")
+                },
+                new Clause(Resultado,igual,"RAVEN-G, BENDER")
+        );
+        Rule reglaIVP14 = new Rule(baseDeReglas, "reglaIVP14",
+                new Clause[]{
+                        new Clause(Area,igual,"IVP"),
+                        new Clause(Edad,menor,"15")
+                },
+                new Clause(Resultado,igual,"RAVEN-G, PMA")
+        );
+        Rule reglaIVP15 = new Rule(baseDeReglas, "reglaIVP15",
+                new Clause[]{
+                        new Clause(Area,igual,"IVP"),
+                        new Clause(Edad,mayor,"14")
+                },
+                new Clause(Resultado,igual,"PMA, RAVEN-C")
         );
 
-        Rule reglaSeis = new Rule(baseDeReglas, "reglaSeis",
-                new Clause[]{
-                    new Clause(Area,igual,"IMO"),
-                        new Clause(Edad,mayor,"6")
-                },
-                    new Clause(Resultado,igual,"Pro-Cálculo")
-        );
 
-        Rule reglaSiete = new Rule(baseDeReglas, "reglaCinco",
-                new Clause[]{
-                    new Clause(Area,igual,"IVP"),
-                        new Clause(Edad,menor,"7")
-                },
-                    new Clause(Resultado,igual,"Pre-Cálculo")
-        );
 
-        Rule reglaOcho = new Rule(baseDeReglas, "reglaSeis",
-                new Clause[]{
-                    new Clause(Area,igual,"IVP"),
-                        new Clause(Edad,mayor,"6")
-                },
-                    new Clause(Resultado,igual,"Pro-Cálculo")
-        );
+
+//
+//        Rule reglaICV1 = new Rule(baseDeReglas, "reglaDos",
+//                new Clause[]{
+//                    new Clause(Area,igual,"ICV"),
+//                        new Clause(Edad,mayor,"9")
+//                },
+//                    new Clause(Resultado,igual,"PROLEC")
+//        );
+//
+//        Rule reglaTres = new Rule(baseDeReglas, "reglaTres",
+//                new Clause[]{
+//                    new Clause(Area,igual,"IRP"),
+//                        new Clause(Edad,menor,"7")
+//                },
+//                    new Clause(Resultado,igual,"BENDER")
+//        );
+//
+//        Rule reglaCuatro = new Rule(baseDeReglas, "reglaCuatro",
+//                new Clause[]{
+//                    new Clause(Area,igual,"IRP"),
+//                        new Clause(Edad,mayor,"6")
+//                },
+//                    new Clause(Resultado,igual,"TEPSI")
+//        );
+//
+//        Rule reglaCinco = new Rule(baseDeReglas, "reglaCinco",
+//                new Clause[]{
+//                    new Clause(Area,igual,"IMO"),
+//                        new Clause(Edad,menor,"7")
+//                },
+//                    new Clause(Resultado,igual,"Pre-Cálculo")
+//        );
+//
+//        Rule reglaSeis = new Rule(baseDeReglas, "reglaSeis",
+//                new Clause[]{
+//                    new Clause(Area,igual,"IMO"),
+//                        new Clause(Edad,mayor,"6")
+//                },
+//                    new Clause(Resultado,igual,"Pro-Cálculo")
+//        );
+//
+//        Rule reglaSiete = new Rule(baseDeReglas, "reglaCinco",
+//                new Clause[]{
+//                    new Clause(Area,igual,"IVP"),
+//                        new Clause(Edad,menor,"7")
+//                },
+//                    new Clause(Resultado,igual,"Pre-Cálculo")
+//        );
+//
+//        Rule reglaOcho = new Rule(baseDeReglas, "reglaSeis",
+//                new Clause[]{
+//                    new Clause(Area,igual,"IVP"),
+//                        new Clause(Edad,mayor,"6")
+//                },
+//                    new Clause(Resultado,igual,"Pro-Cálculo")
+//        );
 
 
 
